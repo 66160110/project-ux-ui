9a6677d0bbd516c8f08dab291f3e9a320fefee1c
Kanatee <66160110@go.buu.ac.th>
2025-03-15 12:29:30 +0700

    DAY5 finish like,save,follow button on homepage

--- javasc.js
+++ javasc.js
@@ -2,3 +2,27 @@
 module.exports = {
     plugins: [require('tailwind-scrollbar-hide')],
   }
+
+  // เปลี่ยนสีปุ่ม button ของ follow and unfollow
+  function toggleFollow(button) {
+    if (button.innerText === "Follow") {
+        button.classList.remove("bg-gray-200");
+        button.classList.add("bg-gradient-to-r", "from-pink-200", "to-gray-400", "text-black");
+        button.innerText = "Unfollow";
+    } else {
+        button.classList.remove("bg-gradient-to-r", "from-pink-200", "to-gray-400", "text-black");
+        button.classList.add("bg-gray-200");
+        button.innerText = "Follow";
+    }
+  }
+
+  function toggleImage(img) {
+    // ดึงตัว src ของปัจจุบัน
+    let currentSrc = img.getAttribute("src"); 
+    // ดึง data-alt-src ที่เอาไว้สลับรูป
+    let altSrc = img.getAttribute("data-alt-src");
+
+    // สลับรูปไปมาระหว่าง active กับ not active
+    img.setAttribute("src",altSrc);
+    img.setAttribute("data-alt-src",currentSrc);
+  }
